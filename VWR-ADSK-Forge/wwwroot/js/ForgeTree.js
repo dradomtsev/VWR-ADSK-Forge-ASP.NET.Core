4e3b92ec89f132520fa9d2b1d493641d835ccb7d
d.radomtsev <d.radomtsev@gmail.com>
2020-02-10 10:59:18 +0200

    reinit prepareAppBucketTree()

--- VWR-ADSK-Forge/wwwroot/js/ForgeTree.js
+++ VWR-ADSK-Forge/wwwroot/js/ForgeTree.js
@@ -151,6 +151,61 @@ function prepareAppBucketTree() {
     });
 }
 
+//function prepareAppBucketTree() {
+//    $('#appBuckets').jstree({
+//        'core': {
+//            'themes': { "icons": true },
+//            'data': {
+//                "url": '/api/forge/oss/buckets',
+//                "dataType": "json",
+//                'multiple': false,
+//                "data": function (node) {
+//                    return { "id": node.id };
+//                }
+//            }
+//        },
+//        'types': {
+//            'default': {
+//                'icon': 'glyphicon glyphicon-question-sign'
+//            },
+//            '#': {
+//                'icon': 'glyphicon glyphicon-cloud'
+//            },
+//            'bucket': {
+//                'icon': 'glyphicon glyphicon-folder-open'
+//            },
+//            'object': {
+//                'icon': 'glyphicon glyphicon-file'
+//            }
+//        },
+//        "plugins": ["types", "state", "sort", "contextmenu"],
+//        contextmenu: { items: autodeskCustomMenu }
+//    }).on('loaded.jstree', function () {
+//        $('#appBuckets').jstree('open_all');
+//    }).bind("activate_node.jstree", function (evt, data) {
+//        if (data != null && data.node != null && data.node.type == 'object') {
+//            $("#forgeViewer").empty();
+//            var urn = data.node.id;
+//            getForgeToken(function (access_token) {
+//                jQuery.ajax({
+//                    url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn + '/manifest',
+//                    headers: { 'Authorization': 'Bearer ' + access_token },
+//                    success: function (res) {
+//                        if (res.status === 'success') launchViewer(urn);
+//                        else $("#forgeViewer").html('The translation job still running: ' + res.progress + '. Please try again in a moment.');
+//                    },
+//                    error: function (err) {
+//                        var msgButton = 'This file is not translated yet! ' +
+//                            '<button class="btn btn-xs btn-info" onclick="translateObject()"><span class="glyphicon glyphicon-eye-open"></span> ' +
+//                            'Start translation</button>'
+//                        $("#forgeViewer").html(msgButton);
+//                    }
+//                });
+//            })
+//        }
+//    });
+//}
+
 function autodeskCustomMenu(autodeskNode) {
     var items;
 
